45ed55dcd2b3ea6c65370d05e758903749f7ea4e
laith awad <layth_awad@hotmail.com>
2019-04-09 21:43:15 -0400

    new

---
+++ tests/sessionClassTest.php
@@ -0,0 +1,24 @@
+<?php
+require "/src/Session.php";
+use PHPUnit\Framework\TestCase;
+
+
+ class conflictChecker extends TestCase
+{
+	public function testConflictExists ()
+	{
+		$s1 = new Session(null, null, null, "Fall", array ("M", "W"), 090000, 100000, null);
+		$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 090000, 100000, null);
+
+		$this->assertFalse(conflictExists($s1,$s2));
+	}
+
+	public function testNoConflictExists ()
+	{
+		$s1 = new Session(null, null, null, "Fall", array ("T", "F"), 090000, 100000, null);
+		$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 090000, 100000, null);
+
+		$this->assertFalse(conflictExists($s1,$s2));
+	}
+}
+?>
