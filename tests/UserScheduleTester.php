cb618b3c88962ffeae1688d764b26aed450d25c3
laith awad <layth_awad@hotmail.com>
2019-04-09 23:35:38 -0400

    a

--- tests/UserScheduleTester.php
+++ tests/UserScheduleTester.php
@@ -7,23 +7,14 @@ use PHPUnit\Framework\TestCase;
 {
 	public function testProgramSchedCorrect ()
 	{
-  //  $userName, $email, $userSchedule, $firstSemester
+
     $userSchedule1 = new UserSchedule(null, null);
 		$User1 = new User("userName", "test@t.com", $userSchedule1 , "Fall");
-		//$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
+  
       $userSchedule1->genProgramSched($User1);
     $listOfSemester1 = $userSchedule1 -> getListOfSemesters();
 		$this->assertGreaterThan(0, count($listOfSemester1));
 	}
-/*
-	public function testNoConflictExists ()
-	{
-		$s1 = new Session(null, null, null, "Fall", array ("T", "F"), 90000, 100000, null);
-		$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
-
-		$this->assertFalse(conflictExists($s1,$s2));
-	}
-*/
 
 }
 ?>
