d96abb5c7c4cef61d726106ce70d9823f3c23ebd
Osama <osama.qalam@hotmail.com>
2019-04-09 23:03:54 -0400

    end session test

--- tests/sessionClassTest.php
+++ tests/sessionClassTest.php
@@ -3,9 +3,17 @@
 use PHPUnit\Framework\TestCase;
 //require_once __DIR__.'/../src/Session.php';
 
- class conflictChecker extends TestCase
+ class sessionClassTester  extends TestCase
 {
-	public function testConflictExists ()
+  public function testNoConflictExists ()
+	{
+		$s1 = new Session(null, null, null, "Fall", array ("T", "F"), 90000, 100000, null);
+		$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
+
+		$this->assertFalse(conflictExists($s1,$s2));
+	}
+
+	public function testConflictExistsCompleteOverlap ()
 	{
 		$s1 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
 		$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
@@ -13,12 +21,29 @@ use PHPUnit\Framework\TestCase;
 		$this->assertTrue(conflictExists($s1,$s2));
 	}
 
-	public function testNoConflictExists ()
-	{
-		$s1 = new Session(null, null, null, "Fall", array ("T", "F"), 90000, 100000, null);
-		$s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
+  public function testConflictExistsOverlapAtLeftSide ()
+ {
+   $s1 = new Session(null, null, null, "Fall", array ("M", "W"), 80000, 93000, null);
+   $s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
 
-		$this->assertFalse(conflictExists($s1,$s2));
-	}
+   $this->assertTrue(conflictExists($s1,$s2));
+ }
+
+ public function testConflictExistsOverlapAtRightSide ()
+{
+  $s1 = new Session(null, null, null, "Fall", array ("M", "W"), 93000, 103000, null);
+  $s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
+
+  $this->assertTrue(conflictExists($s1,$s2));
+}
+
+public function testConflictExistsOverlapOneDay ()
+{
+ $s1 = new Session(null, null, null, "Fall", array ("M"), 93000, 103000, null);
+ $s2 = new Session(null, null, null, "Fall", array ("M", "W"), 90000, 100000, null);
+
+ $this->assertTrue(conflictExists($s1,$s2));
+}
+
 }
 ?>
