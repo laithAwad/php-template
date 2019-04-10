097c06ed47a2dd12c55a9ece79bec9bd33bdcb29
laith awad <layth_awad@hotmail.com>
2019-04-09 23:09:27 -0400

    Update UserSchedule.php

--- src/UserSchedule.php
+++ src/UserSchedule.php
@@ -4,7 +4,6 @@ require_once __DIR__.'/Databases/DBinterface/DBinterface.php';
 require_once 'Session.php';
 require_once 'Course.php';
 require_once 'Semester.php';
-require_once 'heapSort.php';
 require_once 'User.php';
 
 $DEFAULT_COURSES_PER_SEM = 4;
